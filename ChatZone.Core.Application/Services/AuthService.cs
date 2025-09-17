8aa016bc10893ed0f9312629e3674553dbb6de6e
Dawid Szymik <dawid_szymik@wp.pl>
2025-09-17 13:50:08 +0200

    Add user registration functionality and DTOs
    - Updated `ChatZone.Core.Application.csproj` to reference `ChatZone.Core.Domain.csproj`.
    - Added constructor to `User` class for property initialization.
    - Introduced `AuthService` class for user registration logic with logging.
    - Created `RegisterUserDto` class for transferring registration data.

---
+++ ChatZone.Core.Application/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using ChatZone.Core.Domain.Dtos;
+using ChatZone.Core.Domain.Interfaces.Repositories;
+using ChatZone.Core.Domain.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatZone.Core.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<AuthService> _logger;
+
+        public AuthService(IUserRepository userRepository, ILogger<AuthService> logger) {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public async Task RegisterUser(RegisterUserDto registerUser) {
+            try {
+                var existingUser = _userRepository.GetUserByLogin(registerUser.Username);
+                if (existingUser != null) {
+                    _logger.LogWarning($"User with login: {registerUser.Username} already exists");
+                    throw new InvalidOperationException("User with this username already exists.");
+                }
+
+                var user = new User(registerUser.Username, registerUser.Password);
+                await _userRepository.AddUser(user);
+            }
+            catch (Exception ex) {
+                _logger.LogError(ex, $"Error occurred while registering user with login: {registerUser.Username}");
+                throw new InvalidProgramException();
+            }
+        }
+    }
+}
--- ChatZone.Core.Domain/Models/User.cs
+++ ChatZone.Core.Domain/Models/User.cs
@@ -8,6 +8,13 @@ namespace ChatZone.Core.Domain.Models
 {
     public class User
     {
+        public User(string username, string password) {
+            Id = Guid.NewGuid();
+            Username = username;
+            Password = password;
+            CreatedAt = DateTime.UtcNow;
+        }
+
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
