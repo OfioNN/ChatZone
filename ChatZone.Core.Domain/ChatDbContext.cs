db69c97be0e24c64a47d1386f8cdfe2f151acab8
Dawid Szymik <dawid_szymik@wp.pl>
2025-09-17 20:28:22 +0200

    Add Chat and Message entities with relationships
    Updated ChatDbContext to include DbSet properties for Chat and Message. Enhanced OnModelCreating to define relationships and seed a default Chat instance. Modified User class to include a collection of Messages. Introduced new Chat and Message classes with necessary properties and navigation properties for relationships.

--- ChatZone.Core.Domain/ChatDbContext.cs
+++ ChatZone.Core.Domain/ChatDbContext.cs
@@ -14,5 +14,28 @@ namespace ChatZone.Core.Domain
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Chat)
+                .WithMany(m => m.Messages)
+                .HasForeignKey(m => m.ChatId);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.Messages)
+                .HasForeignKey(m => m.SenderId);
+
+            modelBuilder.Entity<Chat>().HasData(
+                new Chat {
+                    ChatId = Guid.NewGuid(),
+                    Name = "Global",
+                    CreatedAt = DateTime.UtcNow
+                }
+            );
+        }
     }
 }
