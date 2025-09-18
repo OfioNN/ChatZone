cf333b945b0753f3e968ec8a6254ca3c10339809
Dawid Szymik <dawid_szymik@wp.pl>
2025-09-18 09:55:36 +0200

    Enhance MessageHub for chat group management
    Updated MessageHub.cs to support user connections in chat groups.
    Added JWT claims handling with a new _userId property.
    Refactored OnConnectedAsync and OnDisconnectedAsync to be asynchronous, enabling group join/leave functionality.
    Introduced JoinChat and LeaveChat methods for managing group memberships.

--- ChatZone.API/Hubs/MessageHub.cs
+++ ChatZone.API/Hubs/MessageHub.cs
@@ -1,6 +1,7 @@
 ﻿using ChatZone.Core.Application.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace ChatZone.API.Hubs {
@@ -9,20 +10,32 @@ namespace ChatZone.API.Hubs {
 
         private readonly UserConnectionService _userConnectionService;
         private string _username => _userConnectionService.GetClaimValue(Context.User, ClaimTypes.NameIdentifier);
+        private string _userId => _userConnectionService.GetClaimValue(Context.User, JwtRegisteredClaimNames.Jti);
+        private const string _mainChat = "global";
 
         public MessageHub(UserConnectionService userConnectionService) {
             _userConnectionService = userConnectionService;
         }
 
-        public override Task OnConnectedAsync() {
+        public override async Task OnConnectedAsync() {
             
             _userConnectionService.AddConnection(_username, Context.ConnectionId);
-            return base.OnConnectedAsync();
+            await JoinChat(_mainChat);
+            await base.OnConnectedAsync();
         }
 
-        public override Task OnDisconnectedAsync(Exception? exception) {
+        public override async Task OnDisconnectedAsync(Exception? exception) {
             _userConnectionService.RemoveConnection(_username);
-            return base.OnDisconnectedAsync(exception);
+            await LeaveChat(_mainChat);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private async Task JoinChat(string chatName) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatName);
+        }
+
+        private async Task LeaveChat(string mainChat) {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, mainChat);
         }
     }
 }
